e996e27d0ccef0d9541864eaddde1c6e471c13f3
David Reynolds <dr14598@my.bristol.ac.uk>
2024-10-08 08:44:33 +0000

    Linked git and heroku

--- learn_django/settings.py
+++ learn_django/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k19sl$dy62x%7p(1g@jxg^$o-@ap*j=or6ms87pdj6*+5+k(h)'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-k19sl$dy62x%7p(1g@jxg^$o-@ap*j=or6ms87pdj6*+5+k(h)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dr-django-todo-app-215e5ac1c2e1.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
